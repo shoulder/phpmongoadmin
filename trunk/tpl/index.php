d35bb83d86c9edaa8522187df0614bcf2f2119ad
dualface <dualface@61bd00d2-dd82-11de-b4bf-3fb2a63344f9>
2010-01-16 00:03:09 +0000

    * add pagination

--- trunk/tpl/index.php
+++ trunk/tpl/index.php
@@ -5,11 +5,9 @@
 <title>phpMongoAdmin</title>
 <link href="<?php echo $_BASE_DIR; ?>/media/twoColElsLtHdr.css" rel="stylesheet" type="text/css" />
 <!--[if IE]>
-<style type="text/css"> 
-/* 请将所有版本的 IE 的 css 修复放在这个条件注释中 */
+<style type="text/css">
 .twoColElsLtHdr #sidebar1 { padding-top: 30px; }
 .twoColElsLtHdr #mainContent { zoom: 1; padding-top: 15px; }
-/* 上面的专用 zoom 属性为 IE 提供避免错误所需的 hasLayout */
 </style>
 <![endif]-->
 </head>
@@ -50,14 +48,30 @@
       </tr>
     <?php endwhile; ?>
     </table>
-    
-    <p>doc count: <?php echo $cur->count(); ?>, skip: <?php echo $skip; ?>, limit: <?php echo $limit; ?></p>
-    
-    
+
+    <p>doc count: <?php $count = $cur->count(); echo $count; ?>, skip: <?php echo $skip; ?>, limit: <?php echo $limit; ?></p>
+    <p>
+      <?php
+      $url_args = array('coll' => $current_coll->getName(), 'skip' => 0);
+      for ($i = 0; $i < intval($count / $limit); $i++):
+      ?>
+
+      <?php if ($i * $limit == $skip): ?>
+
+      [<?php echo ($i + 1); ?>]
+
+      <?php else: ?>
+
+      <a href="<?php $url_args['skip'] = $i * $limit; echo url('default.index', $url_args); ?>">[<?php echo ($i + 1); ?>]</a>
+
+      <?php endif; ?>
+
+      <?php endfor; ?>
+    </p>
+
     <!-- end #mainContent -->
     <?php endif; ?>
 </div>
-  <!-- 这个用于清除浮动的元素应当紧跟 #mainContent div 之后，以便强制 #container div 包含所有的子浮动 -->
   <br class="clearfloat" />
   <div id="footer">
     <p>Powered by MongoDB</p>
